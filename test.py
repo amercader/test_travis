07a3d61018021b084b72a904ad3c1099ecb67d27
amercader <amercadero@gmail.com>
2015-11-17 09:24:14 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+import nose
+
+assert_equals = nose.tools.assert_equals
+
+
+def test_py_version_26():
+
+    assert_equals(sys.version_info[0], 2)
+    assert_equals(sys.version_info[1], 6)
+
+
+def test_py_version_27():
+
+    assert_equals(sys.version_info[0], 2)
+    assert_equals(sys.version_info[1], 7)
